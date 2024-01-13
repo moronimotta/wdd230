d9e7e732d85e681c47863476f7ad93304ed6614f
Moroni Motta <dareevil7@gmail.com>
2024-01-13 18:32:21 -0300

    w1

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+let currentYear = document.getElementById("currentYear").innerHTML = "&copy; " + new Date().getFullYear() + " - Moroni Motta - Brazil";
+let lastModified = document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+
