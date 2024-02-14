a8640c613f3dd77fcfecc044bd39df46c0c838eb
Moroni Motta <dareevil7@gmail.com>
2024-02-14 15:52:30 -0300

    BOM

---
+++ lesson05/scripts/chapter-handler.js
@@ -0,0 +1,63 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.getElementById("list");
+const li = document.createElement('li');
+const deleteButton = document.createElement('button')
+
+
+deleteButton.addEventListener('click', () => {
+    list.removeChild(li);
+    input.focus();
+})
+
+button.addEventListener('click', () => {
+    if (!input.value) {
+        alert("You cant submit a blank chapter")
+        return input.focus()
+    }
+
+    handleInput();
+
+    // li.textContent = input.value;
+    // deleteButton.textContent = '❌'
+    // li.append(deleteButton)
+
+    // list.append(li)
+
+    // input.value = '';
+    // input.focus()
+
+})
+
+function handleInput() {
+    bookList.forEach((book)=>{
+
+        title = book.section.title;
+        title = title.toLowerCase();
+        input.value = input.value.toLowerCase();
+        chapters = book.section.entries;
+    
+        if(title.includes(input.value)){
+            chapterInput = title.replace(/\D/g,'');
+            chapterCount = chapters.length - 1;
+           
+            if(chapterInput > chapterCount){
+                alert("Chapter not found");
+                return input.focus();
+            }
+            li.textContent = chapter[chapterInput].content.title;
+            deleteButton.textContent = '❌'
+            li.append(deleteButton)
+            list.append(li)
+            input.value = '';
+            input.focus()
+        }else {
+            alert("Book not found");
+            return input.focus();
+        }
+    }
+    )
+
+}
+
+
---
+++ lesson05/scripts/scriptures-books.js
@@ -0,0 +1,25 @@
+var bookList =  [];
+
+const request = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+     },
+     mode : 'cors',
+     cache: 'default'
+}
+
+fetch("https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/dynamic?lang=eng&uri=/scriptures/bofm", request)
+.then((data)=>{
+    bookList = data.toc.entries;
+}).catch((error)=>{
+    alert(error);
+});
+
+
+
+
+
