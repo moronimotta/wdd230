52890a88190b1d913a97f34bc1678fc90e6e0e7b
Moroni Motta <dareevil7@gmail.com>
2024-02-16 20:41:46 -0300

    bom assigment redo

---
+++ lesson05/scripts/favoriteChapters.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = '';
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        }
+        );
+    }else {
+        alert('Please enter a favorite chapter.');
+        
+    }
+    input.focus();
+}
+);
+
+
