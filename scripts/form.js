15829df6984f73641add0c3277dead5f34e50048
Moroni Motta <dareevil7@gmail.com>
2024-03-09 20:50:58 -0300

    fix w8

--- scripts/form.js
+++ scripts/form.js
@@ -8,3 +8,33 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+function checkPasswordMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+    var message = document.getElementById("password-match-message");
+
+    if (password === confirmPassword) {
+        message.innerHTML = "Passwords match";
+        message.style.color = "green";
+    } else {
+        message.innerHTML = "Passwords do not match";
+        message.style.color = "red";
+    }
+}
+
+
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var emailValidationMessage = document.getElementById("email-validation-message");
+
+    if (/^[a-zA-Z0-9._%+-]+@byui\.edu$/.test(email)) {
+        emailValidationMessage.innerHTML = "Valid BYUI email address";
+        emailValidationMessage.style.color = "green";
+        return true;
+    } else {
+        emailValidationMessage.innerHTML = "Please enter a valid BYUI email address";
+        emailValidationMessage.style.color = "red";
+        return false;
+    }
+}
