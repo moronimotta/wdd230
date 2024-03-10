12dd9cec6afeb053099a5767720daa0e89403bbc
Moroni Motta <dareevil7@gmail.com>
2024-03-09 21:54:15 -0300

    fix chamber

--- chamber/scripts/forms.js
+++ chamber/scripts/forms.js
@@ -2,4 +2,4 @@ var formLoadedTime = Date.now();
 
 formLoadedTime = new Date(formLoadedTime).toLocaleString();
 document.getElementById("form").value = formLoadedTime;
-alert("Form loaded at " + formLoadedTime);
+
