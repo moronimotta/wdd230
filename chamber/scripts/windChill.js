35cd7afe8b02828eb8e5ead6977eba5e769c0f4b
Moroni Motta <dareevil7@gmail.com>
2024-03-21 22:49:27 -0300

    final adjustments

--- chamber/scripts/windChill.js
+++ chamber/scripts/windChill.js
@@ -1,16 +1,17 @@
-let temperature = document.querySelector('.temperature');
-let windSpeed = document.querySelector('.windSpeed');
-let windChill = document.querySelector('.windChill');
+let temperatureInput = document.querySelector('#temperature-windchill');
+let windSpeedInput = document.querySelector('#windSpeed');
+let windChillOutput = document.querySelector('.windChill');
 
-temperature = parseFloat(temperature.textContent);
-windSpeed = parseFloat(windSpeed.textContent.split(' ')[1]);
-let formula = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-formula = Math.round(formula * 100) / 100;
-
-windChill.textContent = "Wind Chill: " + formula + "°";
-if (temperature <= 50 && windSpeed > 3) {
-    windChill.textContent = "Wind Chill: " + formula + "°";
-}
-else {
-    windChill.textContent = "Wind Chill: N/A";
+function calculateWindChill() {
+    let temperature = parseFloat(temperatureInput.value);
+    let windSpeed = parseFloat(windSpeedInput.value);
+    let formula = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    formula = Math.round(formula * 100) / 100;
+    
+    if (temperature <= 50 && windSpeed > 3) {
+        windChillOutput.textContent = "Wind Chill: " + formula + "°C";
+    }
+    else {
+        windChillOutput.textContent = "Wind Chill: N/A";
+    }
 }
