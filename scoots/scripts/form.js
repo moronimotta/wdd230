d3233ee3a77519fa1a2eac1470195637fddde5d8
Moroni Motta <dareevil7@gmail.com>
2024-04-08 00:25:10 -0300

    logical parts done

--- scoots/scripts/form.js
+++ scoots/scripts/form.js
@@ -1,6 +1,11 @@
+const baseURL = "https://moronimotta.github.io/wdd230/"
+const linksURL = baseURL + "scoots/data/rentals.json";
+
+storedData = [];
+
 function selectMotor(type) {
     document.getElementById('selectedType').value = type;
-    
+
     const icons = document.querySelectorAll('.motor-icon');
     icons.forEach(icon => {
         if (icon.alt.toLowerCase() === type) {
@@ -11,67 +16,158 @@ function selectMotor(type) {
     });
 }
 
- function unlockForm() {
+function unlockForm() {
     var agreementCheckbox = document.getElementById('agreement');
     var formInputs = document.querySelectorAll('form input, form select, form textarea');
-    
+
     if (agreementCheckbox.checked) {
-        formInputs.forEach(function(input) {
+        formInputs.forEach(function (input) {
             input.removeAttribute('disabled');
         });
     } else {
-        formInputs.forEach(function(input) {
+        formInputs.forEach(function (input) {
             input.setAttribute('disabled', 'disabled');
         });
     }
 }
 
+function formInitalValue() {
+    var formInputs = document.querySelectorAll('form input, form select, form textarea');
+
+    formInputs.forEach(function (input) {
+        input.setAttribute('disabled', 'disabled');
+    });
+}
+
+formInitalValue();
+
+const models = {
+    scooter: [],
+    atv: [],
+    jeep: []
+};
 
 async function getRentals() {
     try {
         const response = await fetch(linksURL);
         const data = await response.json();
 
+        data.rentals.forEach(rental => {
+            if (models.hasOwnProperty(rental.type)) {
+                models[rental.type].push(rental.name);
+            }
+        });
+
+        storedData = data.rentals;
+
     } catch (error) {
         console.error(error);
     }
 }
-
-
 getRentals();
 
 
-const models = {
-    scooter: ["Honda Metro Scooter", "Honda Dio Scooter", "Honda PCX150 Scooter"],
-    atv: ["Honda Pioneer ATV"],
-    jeep: ["Jeep Wrangler - 4 door with a/c", "Jeep Wrangler - 2 door"]
-};
+function populateOptionsAndPeriod() {
+    const modelTypeSelect = document.getElementById('model-type');
+    const periodSelect = document.getElementById('period');
+    const selectedModel = modelTypeSelect.value;
+
+    const rental = storedData.find(item => item.name === selectedModel);
+
+    if (rental) {
+        periodSelect.innerHTML = '';
+
+        const halfDayOption = document.createElement('option');
+        halfDayOption.value = 'half-day';
+        halfDayOption.textContent = `Half Day - $${rental.halfDayPrice}`;
+        periodSelect.appendChild(halfDayOption);
+
+        const fullDayOption = document.createElement('option');
+        fullDayOption.value = 'full-day';
+        fullDayOption.textContent = `Full Day - $${rental.fullDayPrice}`;
+        periodSelect.appendChild(fullDayOption);
+    }
+}
 
 function populateModelOptions() {
-    var selectedType = document.querySelector('input[name="motorType"]:checked').value;
-
-    var selectedModels = models[selectedType];
-
-    var modelTypeSelect = document.getElementById('model-type');
+    const selectedType = document.querySelector('input[name="motorType"]:checked').value;
+    const selectedModels = models[selectedType];
+    const modelTypeSelect = document.getElementById('model-type');
 
     modelTypeSelect.innerHTML = '';
 
-    selectedModels.forEach(function(model) {
+    selectedModels.forEach(function (model) {
         var option = document.createElement('option');
         option.value = model;
         option.textContent = model;
         modelTypeSelect.appendChild(option);
     });
+
+    populateOptionsAndPeriod();
 }
 
-function handleRentalTypeChange() {
-    populateModelOptions();
-}
+const rentalTypeRadios = document.querySelectorAll('input[name="motorType"]');
+rentalTypeRadios.forEach(function (radio) {
+    radio.addEventListener('change', function () {
+        populateModelOptions();
+    });
+});
 
-var rentalTypeRadios = document.querySelectorAll('input[name="motorType"]');
-rentalTypeRadios.forEach(function(radio) {
-    radio.addEventListener('change', handleRentalTypeChange);
+const modelTypeSelect = document.getElementById('model-type');
+modelTypeSelect.addEventListener('change', function () {
+    populateOptionsAndPeriod();
 });
 
 populateModelOptions();
+
+
+function updateTotalCost() {
+    const periodSelect = document.getElementById('period');
+    const numberInput = document.getElementById('number');
+    const confirmInput = document.getElementById('confirmInput');
+
+    if (periodSelect.value && numberInput.value) {
+        confirmInput.style.display = 'block';
+    } else {
+        confirmInput.style.display = 'none';
+    }
+}
+
+function displayTotalCost() {
+    const periodSelect = document.getElementById('period');
+    const numberInput = document.getElementById('number');
+    const totalFieldset = document.getElementById('totalFieldset');
+    const totalCostParagraph = document.getElementById('total-cost');
+
+    const selectedPeriod = periodSelect.value;
+    const numberRentals = parseInt(numberInput.value);
+
+    let rentalPrice = 0;
+    if (selectedPeriod === 'half-day') {
+        rentalPrice = parseFloat(periodSelect.querySelector('option[value="half-day"]').textContent.match(/\d+/)[0]);
+    } else if (selectedPeriod === 'full-day') {
+        rentalPrice = parseFloat(periodSelect.querySelector('option[value="full-day"]').textContent.match(/\d+/)[0]);
+    }
+
+    const totalCost = numberRentals * rentalPrice;
+
+    totalCostParagraph.innerHTML = `Total: $${totalCost.toFixed(2)}`;
+    totalFieldset.style.display = 'block';
+}
+
+function confirmDetails() {
+    displayTotalCost();
+}
+
+const numberInput = document.getElementById('number');
+numberInput.addEventListener('input', function() {
+    updateTotalCost();
+});
+
+const periodSelect = document.getElementById('period');
+periodSelect.addEventListener('change', function() {
+    updateTotalCost();
+});
+
+
 
---
+++ scoots/scripts/weather.js
@@ -0,0 +1,69 @@
+const currentTemp = document.querySelector('#current-temp');
+const currentHumidity = document.querySelector('#current-humidity');
+const forecastTemp = document.querySelector('#forecast-temp');
+const maxTempSpan = document.querySelector('#temp-max');
+const messageSection = document.querySelector('#message');
+const closeButton = document.querySelector('#close-message');
+
+const apiKey = 'b954fa2514a069e794c91dede1cb380c';
+const storeLocation = { lat: 20.4225, lon: -86.9223 }; 
+const units = 'imperial';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${storeLocation.lat}&lon=${storeLocation.lon}&units=${units}&appid=${apiKey}`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${storeLocation.lat}&lon=${storeLocation.lon}&units=${units}&appid=${apiKey}`;
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCurrentWeather() {
+    const data = await apiFetch(url);
+    currentTemp.textContent = `${data.main.temp.toFixed(0)}°F`;
+    currentHumidity.textContent = `${data.main.humidity}%`;
+}
+
+async function getNextDayForecast() {
+    const data = await apiFetch(forecastUrl);
+    const forecastAt3PM = data.list.find(entry => {
+        const forecastTime = new Date(entry.dt_txt).getHours();
+        return forecastTime === 15; 
+    });
+
+    if (forecastAt3PM) {
+        forecastTemp.textContent = `${forecastAt3PM.main.temp.toFixed(0)}°F`;
+    } else {
+        forecastTemp.textContent = "Data not available";
+    }
+}
+
+async function getTodayMaxTemp() {
+    const data = await apiFetch(forecastUrl);
+    const temps = data.list.map(entry => entry.main.temp);
+
+    if (temps.length > 0) {
+        const maxTemp = Math.max(...temps);
+        maxTempSpan.textContent = `${maxTemp.toFixed(0)}°F`;
+    } else {
+        maxTempSpan.textContent = "Data not available";
+    }
+}
+
+
+function closeMessage() {
+    messageSection.style.display = 'none';
+}
+
+closeButton.addEventListener('click', closeMessage);
+
+async function displayWeather() {
+    await getCurrentWeather();
+    await getNextDayForecast();
+    await getTodayMaxTemp();
+}
+
+displayWeather();
